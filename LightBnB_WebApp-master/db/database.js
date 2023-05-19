caf750714152f3354d95246c407c7319a9278d4c
Anastasia <z.anastasia11@gmail.com>
2023-05-19 14:20:18 -0700

    replce getAllProperties function with your query code.

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -123,12 +123,58 @@ LIMIT $2;`, [guest_id, limit])
 
 const getAllProperties = (options, limit = 10) => {
 
-  return pool
-    .query(`SELECT * FROM properties LIMIT $1`, [limit])
-    .then((result) => {
-      // console.log(result.rows);
-      return result.rows;
-    })
+  // 1 Setup an array to hold any parameters that may be available for the query.
+  const queryParams = [];
+  // 2 Start the query with all information that comes before the WHERE clause.
+  let queryString = `
+  SELECT properties.*, avg(property_reviews.rating) as average_rating
+  FROM properties
+  JOIN property_reviews ON properties.id = property_id
+  
+  `;
+
+  // 3 Check if a city has been passed in as an option
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    queryString += `WHERE city LIKE $${queryParams.length} `;
+  }
+
+  //if an owner_id is passed in, only return properties belonging to that owner
+ 
+  if (options.owner_id) {
+    queryParams.push(`${options.owner_id}`);
+    queryString += ` AND owner_id = $${queryParams.length} `;
+  }
+
+  // queryString += 'GROUP BY properties.id'
+
+  if (options.minimum_price_per_night) {
+    queryParams.push(`${options.minimum_price_per_night}`);
+    queryString += ` AND cost_per_night > $${queryParams.length} `;
+  }
+
+  if (options.maximum_price_per_night) {
+    queryParams.push(`${options.maximum_price_per_night}`);
+    queryString += ` AND cost_per_night < $${queryParams.length} `;
+  }
+
+  if (options.minimum_rating) {
+    queryParams.push(`${options.minimum_rating}`);
+    queryString += ` AND rating >= $${queryParams.length} `;
+  }
+  // 4 Add any query that comes after the WHERE clause.
+  queryParams.push(limit);
+  queryString += `
+  GROUP BY properties.id
+  ORDER BY cost_per_night
+  LIMIT $${queryParams.length};
+  `;
+
+  // 5 Console log everything just to make sure we've done it right.
+  console.log("queryString", queryString,"queryParams", queryParams);
+
+  // 6
+  return pool.query(queryString, queryParams).then((res) => res.rows)
   .catch((err) => {
     console.log(err.message);
   });
