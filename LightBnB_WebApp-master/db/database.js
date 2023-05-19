45433bd7edb7ac1115f685e632c4da24277d54bd
Anastasia <z.anastasia11@gmail.com>
2023-05-19 10:30:41 -0700

    replace getUserWithId function with your query code.

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -47,6 +47,17 @@ const getUserWithEmail = function (email) {
  */
 const getUserWithId = function (id) {
   return pool
+    .query(`SELECT *
+            FROM users
+            WHERE id = $1`, [id])
+    .then((result) => {
+      console.log("user id", result.rows[0])
+      return result.rows[0];
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+
   // return Promise.resolve(users[id]);
 };
 
